8fe42350e23221e52b884f0a49556aff7fad590d
ilostmykeys <1312troublemaker1312@gmail.com>
2016-05-20 00:36:42 +0300

    Add files via upload

---
+++ resources/js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	
+	var popup = $('#popup');
+	
+	// DISPLAY POPUP AFTER 3 SECONDS
+	
+	timer = setTimeout(function(){
+		$('#popup').css('display','block');
+	}, 3000);
+	
+	// CLOSE POPUP ON CLICK
+	
+	$('#popup-overlay').click(function(){
+		if ( popup.css('display', 'block') ) {
+			popup.css('display', 'none');
+		}
+	});
+	
+	$('#popup-close').click(function(){
+		popup.css('display', 'none');
+	});
+	
+});
