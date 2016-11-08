87db521c38ebc46b467370b95ae2e811083122f5
ilostmykeys <1312troublemaker1312@gmail.com>
2016-11-08 19:10:41 +0200

    Add files via upload

---
+++ projects/rework-emeraldhosting/resources/js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+
+	window.sr = ScrollReveal();
+
+	sr.reveal('#about-index-question');
+	sr.reveal('#product-id-2')
+	sr.reveal('#product-id-1', { delay: 100 });
+	sr.reveal('#product-id-3', { delay: 100 });
+	sr.reveal('#team-index .member-img', 100);
+	sr.reveal('#team-index .container', 100)
+});
